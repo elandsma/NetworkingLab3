40bac7805d4e34e7df2905d1715be45165c1eca7
Elias Landsman <elandsma@unca.edu>
2021-04-13 15:06:44 -0400

    work

--- main.py
+++ main.py
@@ -11,6 +11,57 @@ HOST = '0.0.0.0'
 
 import socket
 import threading
+import queue
+
+list_of_queues = []
+
+def read_lines(conn):
+    '''read from a socket, return array of strings'''
+    buff = ''
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break #reached end of message
+        buff += data.decode('utf-8', "ignore")
+        if buff.endswith("\n"):
+            break
+    print(f"RECV: {format(buff)}")
+    return buff.split("\n")
+
+def reader_thread(conn):
+    lines=[]
+    while True:
+        lines.append (read_lines(conn))
+        while True:
+            msg_start = None
+            msg_end = None
+            #looking for "BEGIN"
+            for i in range(len(lines)):
+                if lines[i]=="BEGIN\n":
+                    print(f"Got BEGIN at line {format(i)}")
+                    msg_start = i
+                    break
+            #lookong for "END"
+            for i in range(len(lines)):
+                if lines[i]=="END":
+                    print(f"got END at line {format(i)}")
+                    msg_end = i
+                    break
+            if msg_start is not None and msg_end is not None:
+                process_message(lines[msg_start:msg_end+1])
+                #remove message portion from lines[] that has already been processed
+                del lines[0:msg_end+1]
+            else:
+                break
+
+def process_message(message_lines):
+    for q in list_of_queues:
+        q.put(message_lines, False)
+
+def writer_thread(conn):
+    while True:
+        msg_to_send = queue.get()
+        conn.sendall(str.encode( "\n".join(msg_to_send) ) )
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,10 +74,32 @@ def main():
             break
         except:
             PORT+=1
-
+    #found an open port.
     try:
         s.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.listen()
         while True:
             # wait  for connection
-    except:
+            conn, addr = s.accept()
+            print(f"New connection from {format(addr)}")
+            #create queue for this client
+            q = queue.Queue(10)
+            list_of_queues.append(q)
+            #create threads to handle connection to client
+            #reader thread
+            tr = threading.Thread(target=reader_thread, args=(conn, ))
+            tr.daemon=True
+            tr.start()
+            #writer thread
+            tw = threading.Thread(target=writer_thread, args=(conn, ))
+            tw.daemon=True
+            tw.start()
+
+
+    finally:
+        s.shutdown(socket.SHUT_WR)
+        s.close()
+
+
+if __name__=="__main__":
+    main()
